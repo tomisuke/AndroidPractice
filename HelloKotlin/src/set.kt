a7ce34cad726cee3b8966e0e7a594558cdfae789
Tomisuke <searakko3@gmail.com>
2024-11-18 15:58:09 +0900

    setの最初の学習

---
+++ HelloKotlin/src/set.kt
@@ -0,0 +1,12 @@
+fun main() {
+    //基本Listと同じ
+    //順番の概念がなく、要素の重複を許可しない。
+    val set:MutableSet<Int> = mutableSetOf(1,2,3,4,5)
+    println(set)
+    set.add(40)
+    set.add(40)
+    //2回addしてもエラーは出ないが、同じ値は1つだけ
+    println(set)
+    //削除
+    set.remove(3)
+}
