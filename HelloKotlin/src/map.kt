be56726fdff572a5c94b23fd06ee0591974f4412
Tomisuke <searakko3@gmail.com>
2024-11-18 16:08:01 +0900

    mapの最初の学習

---
+++ HelloKotlin/src/map.kt
@@ -0,0 +1,23 @@
+fun main() {
+    //キーと要素をセットで管理する
+    //キーの重複は許可しない
+    //可変長
+    //宣言方法
+    //genericsはキーと要素2つ指定する
+    val map = mutableMapOf<String,String>(
+        "orange" to "みかん",
+        "apple" to "りんご",
+        "melon" to "めろん"
+    )//左がキー、右が要素
+    println(map)
+    //変更
+    map["apple"] = "アポー"
+    println(map)
+    //追加
+        //変更と同じ
+    map["banana"] = "バナナ"
+    println(map)
+    //削除
+    map.remove("apple")
+    println(map)
+}
